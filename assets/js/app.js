e260aafea187c619591b724914be87eae363e36a
RiverJAM <thetutorjoemonahan@gmail.com>
2020-12-30 12:04:19 -0600

    adds code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,198 @@
-// @TODO: YOUR CODE HERE!
+// started by copying code from D3 Day 3 Acitvity 12
+
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+
+// only change was to make the id scatter instead of class chart
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "poverty";
+
+// function used for updating x-scale var upon click on axis label
+//changed the data name and associated variables
+function xScale(journalData, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+  //scale adjusted slightly to reduce overlap
+    .domain([d3.min(journalData, d => d[chosenXAxis]) * .9,
+      d3.max(journalData, d => d[chosenXAxis]) * 1.05
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+
+}
+
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  return xAxis;
+}
+
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+
+  return circlesGroup;
+}
+
+// function used for updating circles group with new tooltip
+//was going to add another variable choice for axes but stopped before going through with it
+function updateToolTip(chosenXAxis, circlesGroup) {
+
+  var label;
+
+  if (chosenXAxis === "poverty") {
+    label = "poverty";
+  }
+  else {
+    label = "smokes";
+  }
+
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+      return (`${d.state}<br>${label} ${d[chosenXAxis]} <br> ${label} ${d.healthcare}`);
+    });
+
+  circlesGroup.call(toolTip);
+
+  circlesGroup.on("mouseover", function(data) {
+    toolTip.show(data);
+  })
+    // onmouseout event
+    .on("mouseout", function(data, index) {
+      toolTip.hide(data);
+    });
+
+  return circlesGroup;
+}
+
+// Retrieve data from the CSV file and execute everything below
+d3.csv("/assets/data/data.csv").then(function(journalData, err) {
+  if (err) throw err;
+
+  // parse data
+  journalData.forEach(function(data) {
+      console.log(data)
+    data.poverty = +data.poverty;
+    data.healthcare = +data.healthcare;
+    data.smokes = +data.smokes;
+  });
+
+  // xLinearScale function above csv import
+  var xLinearScale = xScale(journalData, chosenXAxis);
+
+  // Create y scale function
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(journalData, d => d.healthcare) *1.1])
+    .range([height, 0]);
+
+  // Create initial axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // append x axis
+  var xAxis = chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  // append y axis
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // append initial circles
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(journalData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", 11)
+    .attr("fill", "skyblue")
+    .attr("opacity", ".5");
+
+     //https://stackoverflow.com/questions/13615381/d3-add-text-to-circle
+     // the above link was very helpful in getting text into the circles
+    
+    const textelement = chartGroup.append("g")
+    .selectAll('text')
+    .data(journalData)
+    .enter().append('text')
+    .text(function(d){return d.abbr})
+    .attr('font-size',8)//font size
+    .attr('dx', d => xLinearScale(d[chosenXAxis])- 5.5)//positions text towards the left of the center of the circle
+    .attr('dy', d => yLinearScale(d.healthcare) +3 );
+    
+    
+   
+   
+    
+
+  // Create group for two x-axis labels
+  var labelsGroup = chartGroup.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+  var povertyLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 20)
+    .attr("value", "poverty") // value to grab for event listener
+    .classed("active", true)
+    .text("In Poverty (%)");
+
+  // var smokesLabel = labelsGroup.append("text")
+  //   .attr("x", 0)
+  //   .attr("y", 40)
+  //   .attr("value", "smokes") // value to grab for event listener
+  //   .classed("inactive", true)
+  //   .text("smokes (%)");
+
+  // append y axis
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .classed("axis-text", true)
+    .text("Lacks Healthcare (%)");
+
+  // updateToolTip function above csv import
+  var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+ 
+
+}).catch(function(error) {
+  console.log(error);
+});
